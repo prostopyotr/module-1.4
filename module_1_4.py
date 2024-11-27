ea591d34d68ec9432d214c51b13df6ebafa38cbf
prostopyotr <petyaKornienko@yandex.com>
2024-11-27 12:07:27 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,19 @@
+nameOfCourse = 'Python'
+
+numberOfCompletedTasks = 12
+
+numberOfHoursSpent = 1.5
+
+timeForOneTask = 1.5 / 12
+
+print('Курс:', nameOfCourse, 'всего задач:', numberOfCompletedTasks, 'затрачено часов:', numberOfHoursSpent, 'среднее время выполнения', timeForOneTask)
+
+nameOfCourse = 'Python'
+
+numberOfCompletedTasks = 12
+
+numberOfHoursSpent = 1.5
+
+timeForOneTask = 1.5 / 12
+
+print('Курс: {}, всего задач: {}, затрачено часов: {}, среднее время выполнения {}.'. format(nameOfCourse, numberOfCompletedTasks, numberOfHoursSpent, timeForOneTask))
